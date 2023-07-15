e9bd3705efe193d631b39a046e6be95ddd90a0d3
Joris Pieters <joris@jtac.io>
2023-07-15 15:22:46 +0200

    chore: setup modules

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule],
+})
+export class CoreModule {}
