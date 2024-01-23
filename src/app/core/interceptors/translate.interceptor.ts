c11f954fbe743657fd65ccf07ec519ad9a029c67
Joris Pieters <joris@jtac.io>
2024-01-23 15:40:28 +0100

    Feature/distribution (#14)
    * chore: update rss feed
    
    * chore: fix localstorage error ssr
    
    * chore: add logging
    
    * chore: add stuff
    
    * chore: fix translate interceptor

--- src/app/core/interceptors/translate.interceptor.ts
+++ src/app/core/interceptors/translate.interceptor.ts
@@ -3,29 +3,15 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-import { REQUEST } from '@nguniversal/express-engine/tokens';
-import * as express from 'express';
 import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class TranslateInterceptor implements HttpInterceptor {
-  private readonly DEFAULT_PORT: number = null;
-  private readonly PORT = process.env['PORT'] || this.DEFAULT_PORT;
-
-  constructor(@Inject(REQUEST) private request: express.Request) {}
-
-  getBaseUrl(req: express.Request) {
-    const { protocol } = req;
-    return this.PORT
-      ? `${protocol}://${environment.host}:${this.PORT}`
-      : `${protocol}://${environment.host}`;
-  }
-
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     if (request.url.startsWith('./assets')) {
-      const baseUrl = this.getBaseUrl(this.request);
+      const baseUrl = environment.host;
       console.debug('TranslateInterceptor: baseUrl', baseUrl);
       request = request.clone({
         url: `${baseUrl}/${request.url.replace('./assets', 'assets')}`,
