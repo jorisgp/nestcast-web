4c22a5df98b8c69859d794493bacf3907df67651
Joris Pieters <joris@jtac.io>
2024-01-25 22:06:06 +0100

    chore: add fix

--- server.ts
+++ server.ts
@@ -24,13 +24,15 @@ export function app(): express.Express {
     })
   );
 
-  server.get('*', (req, res) => {
+  server.get('*', (req, res, next) => {
     if (req.headers.host.slice(0, 4) === 'www.') {
       var newHost = req.headers.host.slice(4);
       return res.redirect(
         301,
         req.protocol + '://' + newHost + req.originalUrl
       );
+    } else {
+      next();
     }
   });
 
